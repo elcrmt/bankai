d82ff94c0bb42d4433d3ff1b9192219cc861c402
Sebastien H. <sebic1708@gmail.com>
2025-01-22 15:20:09 +0100

    Add Venv in Gitignore
    Add requirements.txt
    Fix main.py bug

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ class BankAccount(SQLModel, table=True):
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    email: EmailStr = Field(unique=True, index=True)
+    email: str = Field(unique=True, index=True)
     password: str
     accounts: List[BankAccount] = Relationship(back_populates="user")
 
